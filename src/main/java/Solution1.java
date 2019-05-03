9254946e6d88f3bc2cee505ba8ebd1001564f45c
jfblg <inbox.jfblg@gmail.com>
2019-05-03 15:11:29 +0200

    add Solution 1

---
+++ src/main/java/Solution1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        scan.close();
+
+        System.out.println("Hello World");
+
+        // Write a line of code here that prints the content of inputString to stdout.
+        System.out.println(inputString);
+
+    }
+}
