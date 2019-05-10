8ccdf88cade5fc90f24af45331d2dbe11e1fea0e
jfblg <inbox.jfblg@gmail.com>
2019-05-10 08:43:17 +0200

    refactor solution 5

---
+++ src/main/java/Solution5/Person.java
@@ -0,0 +1,28 @@
+package Solution5;
+
+public class Person {
+    private int age;
+
+    public Person(int initialAge) {
+
+        if(initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            initialAge = 0;
+        }
+        this.age = initialAge;
+    }
+
+    public void yearPasses() {
+        this.age += 1;
+    }
+
+    public void amIOld() {
+        if(this.age < 13) {
+            System.out.println("You are young.");
+        } else if(this.age >= 13 && this.age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+    }
+}
